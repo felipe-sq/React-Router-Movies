548f8b27c825a3fb936900fe3396385afa5c048e
Felipe Quintero <ffquint@gmail.com>
2021-03-10 17:06:54 -0500

    Third attempt at module project is successful thus far. No major errors to report, but still working on project tasks.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { Route, NavLink, Switch } from 'react-router-dom'
+
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -12,6 +16,8 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          // console.log(response.data)
+          setMovieList(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -30,7 +36,14 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path="/movies/:moviesID">
+          <Movie/>
+        </Route>
+        <Route path="/">
+          <MovieList movies={movieList}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
